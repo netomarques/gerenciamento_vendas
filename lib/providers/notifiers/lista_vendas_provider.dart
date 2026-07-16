701c890b8bea3c1b8fcbfb234adf80070d5d4b93
Wilson Neto <wilsonmarquesg12@gmail.com>
2026-07-16 00:29:54 -0400

    refactor(riverpod): migrate ListaVendasNotifier to Notifier

--- lib/providers/notifiers/lista_vendas_provider.dart
+++ lib/providers/notifiers/lista_vendas_provider.dart
@@ -1,8 +1,7 @@
-import 'package:flutter_riverpod/legacy.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:vendas_gerenciamento/providers/providers.dart';
 
 final listaVendasProvider =
-    StateNotifierProvider<ListaVendasNotifier, ListaVendasState>((ref) {
-  final service = ref.watch(vendaServiceProvider);
-  return ListaVendasNotifier(service);
-});
+    NotifierProvider<ListaVendasNotifier, ListaVendasState>(
+  ListaVendasNotifier.new,
+);
